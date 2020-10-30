3a661247108cc2c9bd83ce090fdb960885a7a2ab
athnme <a.oezbanazi@gmail.com>
2020-10-30 13:57:44 +0100

    something useful

--- src/App.js
+++ src/App.js
@@ -1,10 +1,20 @@
-import React from "react";
+import React, { useState } from "react";
 import "./App.css";
 
+import Form from "./components/Form";
+import List from "./components/List";
+
 function App() {
+  const [inputText, setInputText] = useState("");
+  const [listItem, setListItem] = useState([]);
+
   return (
     <div className="App">
-      <h1>Xmas Wishlist</h1>
+      <header>
+        <h1>Xmas Wishlist</h1>
+      </header>
+      <Form setInputText={setInputText} />
+      <List />
     </div>
   );
 }
